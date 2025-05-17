9e365812b9e347092ed52ec16c53c6587a4dd694
dred974 <guillaume.lallemand@epitech.eu>
2025-05-18 01:46:38 +0400

    ADD: ajout des fonctionnalités dans inscription et connexion

--- front/src/app/connexion/connexion.component.ts
+++ front/src/app/connexion/connexion.component.ts
@@ -3,6 +3,11 @@ import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+interface Login {
+  email?: string,
+  password?: string
+}
+
 @Component({
   selector: 'app-connexion',
   standalone: true,
@@ -11,17 +16,59 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './connexion.component.scss'
 })
 export class ConnexionComponent {
+  user: Login = {}
+  forgetPassword: boolean = false
 
   constructor(private router: Router) {
   }
 
-  toSignInPage(event: Event): void {
-    // event.preventDefault()
+  toSignInPage(): void {
     this.router.navigate(['/inscription'])
   }
 
+  toForgetPasswordForm(b: boolean): void {
+    this.forgetPassword = b
+  }
+
+  sendNewPassword(e: Event, b: boolean): void {
+    e.preventDefault()
+    if (!this.user.password) {
+      alert("Le champ doit être OBLIGATOIREMENT rempli");
+      return;
+    }
+
+    const hasRequiredLength = this.user.password!.length >= 8 && this.user.password!.length <= 40;
+    const hasDigit = /\d/.test(this.user.password!);
+    const hasSpecialChar = /[^a-zA-Z0-9]/.test(this.user.password!);
+
+    if (!hasRequiredLength || !hasDigit || !hasSpecialChar) {
+      alert("Le mot de passe doit contenir entre 8 et 40 caractères, au moins un chiffre et un caractère spécial.");
+      return;
+    }
+
+    this.toForgetPasswordForm(b)
+    this.user.password = ''
+  }
+
   handleSubmit(event: Event): void {
-    event.preventDefault()
-    this.router.navigate(['/home'])
+    event.preventDefault();
+
+    if (!this.user.email || !this.user.password) {
+      alert("Tous les champs doivent être OBLIGATOIREMENT remplis.");
+      return;
+    }
+
+    const hasRequiredLength = this.user.password!.length >= 8 && this.user.password!.length <= 40;
+    const hasDigit = /\d/.test(this.user.password!);
+    const hasSpecialChar = /[^a-zA-Z0-9]/.test(this.user.password!);
+
+
+    if (!hasRequiredLength || !hasDigit || !hasSpecialChar) {
+      alert("Le mot de passe doit contenir entre 8 et 40 caractères, au moins un chiffre et un caractère spécial.");
+      return;
+    }
+
+    // Si tout est bon, on navigue
+    this.router.navigate(['/home']);
   }
 }
--- front/src/app/inscription/inscription.component.ts
+++ front/src/app/inscription/inscription.component.ts
@@ -3,6 +3,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
+interface SignIn {
+  name?: string,
+  email?: string,
+  password?: string
+}
+
 @Component({
   selector: 'app-inscription',
   standalone: true,
@@ -11,6 +17,7 @@ import { Router } from '@angular/router';
   styleUrl: './inscription.component.scss'
 })
 export class InscriptionComponent {
+  user: SignIn = {}
 
   constructor(private router: Router) {
   }
@@ -21,6 +28,22 @@ export class InscriptionComponent {
 
   handleSubmit(event: Event): void {
     event.preventDefault()
+  
+    if (!this.user.name || !this.user.email || !this.user.password) {
+      alert("Tous les champs doivent être OBLIGATOIREMENT remplis.");
+      return;
+    }
+
+    const hasRequiredLength = this.user.password!.length >= 8 && this.user.password!.length <= 40;
+    const hasDigit = /\d/.test(this.user.password!);
+    const hasSpecialChar = /[^a-zA-Z0-9]/.test(this.user.password!);
+
+
+    if (!hasRequiredLength || !hasDigit || !hasSpecialChar) {
+      alert("Le mot de passe doit contenir entre 8 et 40 caractères, au moins un chiffre et un caractère spécial.");
+      return;
+    }
+
     this.router.navigate(['/connexion'])
   }
 }

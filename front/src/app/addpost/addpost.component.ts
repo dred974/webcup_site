2c6a30b557a6b71e8eea31f10e4b591af0f21c1f
dred974 <guillaume.lallemand@epitech.eu>
2025-05-18 03:21:00 +0400

    ADD: ajout de fonctionnalités pour ajout un post

--- front/src/app/addpost/addpost.component.ts
+++ front/src/app/addpost/addpost.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
+interface addPost {
+  title?: string,
+  text?: string
+}
+
 @Component({
   selector: 'app-addpost',
   standalone: true,
@@ -12,10 +17,16 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './addpost.component.scss'
 })
 export class AddpostComponent {
+  post: addPost = {}
 
   constructor(private router: Router) {}
 
   addingPost(): void {
+    if (!this.post.title || !this.post.text) {
+      alert("Tous les champs doivent être OBLIGATOIREMENT remplis")
+      return
+    }
+
     this.router.navigate(['/home'])
   }
 }
